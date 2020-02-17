9e2d1c669f3d23820b334f534ec946125f8be69f
Daiana Bassi <BassiD@gosh.nhs.uk>
2020-02-17 16:22:26 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Deployment Guide
+
+## Install Visual Studio Code
+
+- Download and install [Visual Studio Code](https://code.visualstudio.com/)
+
+## Install .NET Core 2.1
+
+- Download and install .NET Core 2.1 [SDK 2.1.803](https://dotnet.microsoft.com/download/dotnet-core/2.1)
+- Test your installation by opening a new terminal and running the following command:
+
+    ```bash
+    dotnet
+    ```
+
+# Running the app
+
+- Clone the project and open it in Visual Studio Code.
+- Open the file **appsettings.json**
+
+
+    ```json
+    {
+      "Logging": {
+        "LogLevel": {
+          "Default": "Debug",
+          "System": "Information",
+          "Microsoft": "Information"
+        }
+      },
+      "Instance": "",
+      "Tenant": "",
+      "ClientId": "",
+      "ClientSecret": "",
+      "BaseAddress": "",
+      "Scope": ""
+    }
+    ```
+- Replace the empty fields with the Azure FHIR API credentials you have been given.
+- Save the file.
+- Navigate to the directory **dotnet-azure-fhir-web-api** using the terminal inside Visual Studio Code.
+- In the terminal, run the following command:
+
+    ```bash
+    dotnet run
+    ```
+- Open a web browser and navigate to [https://localhost:5001/api/Patient/](https://localhost:5001/api/Patient/) to view a list of all patients.
+
+
+## List of API endpoints
+
+#### Patients
+
+- GET all patients: **/api/Patient**
+- GET a patient: **/api/Patient/<patient ID>**
+- GET a selected number of pages of patient: **api/pages/<number of pages>**
+
+
+#### Observations
+
+- GET all observations for a patient: **/api/Observation/<patient ID>**
+- GET a single observation for a patient: **api/Observation/single/<observation ID>**
+- GET a selected number of pages of observations for a patient: **api/Observation/pages/<number of pages>/<patient ID>**
+
+
+# Adding more controllers
+
+The app uses Dependency Injection (DI) design pattern. For more information on how to implement that on .NET Core check [this article](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1)
+
+
+# Logging and debugging
+
+Check [this article](https://code-maze.com/net-core-web-development-part3/) for an overview of the implemented logging features.
---
+++ dotnet-azure-fhir-web-api/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+using HDR_UK_Web_Application.IServices;
+using HDR_UK_Web_Application.Services;
+using HDR_UK_Web_Application.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using NLog;
+
+namespace HDR_UK_Web_Application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+        }
+
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMemoryCache();
+            services.AddHttpClient();
+
+            services.AddHttpClient("protectedapi", options =>
+            {
+                options.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            });
+
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+            services.AddSingleton<IAccessTokenService, AccessTokenService>();
+            services.AddScoped<IProtectedWebApiCallerService, ProtectedWebApiCallerService>();
+            services.AddTransient<IResourceFetchService, ResourceFetchService>();
+            services.AddTransient<IPatientService, PatientService>();
+            services.AddTransient<IObservationService, ObservationService>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins, builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            { 
+                app.UseHsts();
+            }
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+
+        }
+    }
+}
